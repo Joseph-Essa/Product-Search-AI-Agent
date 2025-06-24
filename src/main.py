82960e56c1f756b1f043d65f0e9b0885bb4d3e22
Joseph-Essa <josepheissa0@gmail.com>
2025-06-25 02:07:24 +0300

    modify agentops end sesion and add search_engine route

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-from helpers.agentops_config import AgentOpsClient
+from clients import AgentOpsClient 
 from routes import base
-from routes import search_queries
+from routes import search_queries, search_engine
+import agentops
 
 
 app = FastAPI()
@@ -9,13 +10,13 @@ agent_ops_client = AgentOpsClient()
 
 async def startup_span():
     print("Starting up...")
-    agent_ops_client._initialize_agentops()
+    agent_ops_client.initialize_agentops()
     print("AgentOps initialized")
 
 
 async def shutdown_span():
     print("Shutting down...")
-    agent_ops_client.end_session()
+    agentops.end_trace()
     print("Shutdown complete")
 
 
@@ -25,3 +26,5 @@ app.on_event("shutdown")(shutdown_span)
 
 app.include_router(base.base_router)
 app.include_router(search_queries.search_queries_router)
+app.include_router(search_engine.search_engine_router)
+
