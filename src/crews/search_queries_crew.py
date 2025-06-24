ea383984411d29e71ce325966faf7a4ffcfa9d35
Joseph-Essa <josepheissa0@gmail.com>
2025-06-25 02:11:19 +0300

    delete company info

--- src/crews/search_queries_crew.py
+++ src/crews/search_queries_crew.py
@@ -1,14 +1,7 @@
 from crewai import Crew, Process
 from agents.search_queries_agent import search_queries_recommendation_agent
 from tasks.search_queries_task import search_queries_recommendation_task
-from crewai.knowledge.source.string_knowledge_source import StringKnowledgeSource
-
-about_company = (
-    "JEBE is a company that provides AI solutions to help websites "
-    "refine their search and recommendation systems."
-)
-
-company_context = StringKnowledgeSource(content=about_company)
+from crews.base import company_context
 
 
 search_queries_crew = Crew(
