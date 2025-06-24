dd1b0ee9193e286d67af4d315050fa3017027780
Joseph-Essa <josepheissa0@gmail.com>
2025-06-24 04:48:06 +0300

    create search route

---
+++ src/routes/search_queries.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter
+from fastapi.responses import JSONResponse
+from models.input import InputModel
+from crews.search_queries_crew import search_queries_crew
+
+search_queries_router = APIRouter(
+    prefix="/api/v1",
+    tags=["api_v1","SearchQueries"]
+)
+
+@search_queries_router.post("/SearchQueries")
+async def search_queries_pipeline(payload: InputModel):
+    crew_results = search_queries_crew.kickoff(inputs=payload.model_dump())
+
+    return JSONResponse(
+        status_code=200,
+        content={
+            "message": "Search queries pipeline completed successfully.",
+            "result": crew_results.model_dump()
+        }
+    )
