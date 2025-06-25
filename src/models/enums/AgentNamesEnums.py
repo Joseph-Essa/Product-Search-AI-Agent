0e06dbd5121cd5599885b06beb81080884114cb1
Joseph-Essa <josepheissa0@gmail.com>
2025-06-26 02:08:36 +0300

    add AGENT enums

--- src/models/enums/AgentNamesEnums.py
+++ src/models/enums/AgentNamesEnums.py
@@ -4,5 +4,5 @@ from enum import Enum
 class AgentNames(str, Enum):
     SEARCH_QUERIES_RECOMMENDATION_AGENT = "search_queries_recommendation_agent"
     SEARCH_ENGINE_AGENT = "Search_Engine_Agent"
-    # WRITER = "writer"
-    # REVIEWER = "reviewer"
+    WEB_SCRAPING_AGENT = "Web Scraping Agent"
+    REPORT_AUTHOR_AGENT = "report_author_agent"
