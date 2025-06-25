150a20005a303556fe92c1984d76d5bc9867beaa
Joseph-Essa <josepheissa0@gmail.com>
2025-06-26 02:07:48 +0300

    update

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -5,10 +5,12 @@ class Settings(BaseSettings):
     APP_NAME: str 
     APP_VERSION: str
     AGENTOPS_API_KEY: str 
-    GEMINI_API_KEY : str 
+    # GEMINI_API_KEY : str 
     TAVILY_API_KEY : str 
     LLM_MODEL_ID : str
     LLM_MODEL_TEMPERATURE : float
+    SCRAPEGRAPHAI_API_KEY : str
+    GROQ_API_KEY : str
     class Config:
         env_file = ".env"
 
