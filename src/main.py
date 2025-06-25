fd63b9a826ab3c74be33ab789b5f2843a4d22088
Joseph-Essa <josepheissa0@gmail.com>
2025-06-26 02:05:29 +0300

    add web_scraping and report_author routes

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from clients import AgentOpsClient 
 from routes import base
-from routes import search_queries, search_engine
+from routes import search_queries, search_engine , web_scraping , report_author
 import agentops
 
 
@@ -27,4 +27,6 @@ app.on_event("shutdown")(shutdown_span)
 app.include_router(base.base_router)
 app.include_router(search_queries.search_queries_router)
 app.include_router(search_engine.search_engine_router)
+app.include_router(web_scraping.web_scraping_router)
+app.include_router(report_author.report_author_router)
 
