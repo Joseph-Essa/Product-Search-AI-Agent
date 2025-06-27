ff411cad42f5e05f7c4918b6d15bbf51484a2d8d
Joseph-Essa <josepheissa0@gmail.com>
2025-06-27 03:21:48 +0300

    update Agent Names

--- src/models/enums/AgentNamesEnums.py
+++ src/models/enums/AgentNamesEnums.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 
 class AgentNames(str, Enum):
-    SEARCH_QUERIES_RECOMMENDATION_AGENT = "search_queries_recommendation_agent"
+    SEARCH_QUERIES_AGENT = "Search_Queries_Agent"
     SEARCH_ENGINE_AGENT = "Search_Engine_Agent"
-    WEB_SCRAPING_AGENT = "Web Scraping Agent"
-    REPORT_AUTHOR_AGENT = "report_author_agent"
+    WEB_SCRAPING_AGENT = "Web_Scraping_Agent"
+    REPORT_AUTHOR_AGENT = "Report_Author_Agent"
